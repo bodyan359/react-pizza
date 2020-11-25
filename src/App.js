bd0ab87a6464cc26dc52f8beb59e8202b70cf10c
Bohdan Yukhymchuk <yukhymchb@gmail.com>
2020-11-25 21:28:00 +0100

    pizzablock

--- src/App.js
+++ src/App.js
@@ -6,12 +6,23 @@ import { Home, Cart } from './pages';
 //console.log(logoSvg);
 
 function App() {
+const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('http://localhost:3000/db.json')
+    .then((resp) => resp.json())
+    .then(json => {
+      setPizzas(json.pizzas);
+    });
+  }, []);
+
+//  console.log(pizzas);
 
   return (
   <div className="wrapper">
     <Header />
     <div className="content">
-    <Route path="/" component={Home} exact/>
+    <Route path="/" render={() => <Home items={pizzas} />} exact/>
     <Route path="/cart" component={Cart} exact/> 
     </div>
   </div>
--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@
     export {default as Categories} from './Categories';
     export {default as Header} from './Header';
     export {default as SortPopup} from './SortPopup';
+    export {default as PizzaBlock} from './PizzaBlock';
 
 
 // import Button from './Button';
