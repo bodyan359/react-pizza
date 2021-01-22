8bac41dfc8d1260fd6e8dd58041020e6ede641a9
Bohdan Yukhymchuk <yukhymchb@gmail.com>
2021-01-22 01:20:29 +0100

    upd

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -87,12 +87,11 @@ function Cart() {
                 <a href="/" className="button button--outline button--add go-back-btn">
                   <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M7 13L1 6.93015L6.86175 1" stroke="#D3D3D3" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-</svg>
+</svg> </a>
 
               <Link to="/">
                 <span>Вернуться назад</span>
               </Link>
-                </a>
                 <Button onClick={onClickOrder} className="pay-btn">
                   <span>Оплатить сейчас</span>
                 </Button >
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const NotFound = () => (
-  <div>
-       <center>
-    <h1>404 - Not Found!</h1>
-         <Link to="/">
-            Go Home
-         </Link>
-    </center>
-  </div>
-);
+function NotFound() {
+  return (
+    <div>
+    <center>
+      <h1>404 - Not Found!</h1>
+        <Link to="/">
+          Go Home
+        </Link>
+    </center> 
+</div>
+  );
+}
 
 export default NotFound;
